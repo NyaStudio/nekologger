1c204f97da3d2b5b1a7f5c2696434af77b5d4a29
BLxcwg666 <huixcwg@gmail.com>
2024-02-22 23:59:37 +0800

    chore: init commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# NekoLogger
+## NekoLogger
 A Simple Node.JS Logger based on Chalk Module
+
+## 使用
+目前有以下五个类: `info`, `ok`, `warn`, `err`, `ing`  
+我定义的寓意是：信息，成功，警告，错误，进行中  
+您可以根据您的理解来使用这些颜色  
+
+基本使用：
+```
+logger.<type>(msg, who);
+```
+
+示例：
+```
+const logger = require('nekologger');
+
+logger.info('这是一条信息', '发出者');
+```
+
+输出：
+```
+[2024-02-22 23:41:06] [发出者] [INFO] 这是一条消息
+```
+
+## 后记
+这玩意是给我自己用的，今后大概不会维护，当个乐子看就行了
---
+++ logger.js
@@ -0,0 +1,40 @@
+//   _   _      _           _
+//  | \ | | ___| | _____   | |    ___   __ _  __ _  ___ _ __
+//  |  \| |/ _ \ |/ / _ \  | |   / _ \ / _` |/ _` |/ _ \ '__|
+//  | |\  |  __/   < (_) | | |__| (_) | (_| | (_| |  __/ |
+//  |_| \_|\___|_|\_\___/  |_____\___/ \__, |\__, |\___|_|
+//                                     |___/ |___/
+// A Simple Node.JS Logger based on Chalk Module
+// By BLxcwg666 <huixcwg@gmail.com>
+// Start at 2024/02/22 23:10 CST
+
+import chalk from "chalk";
+import moment from "moment-timezone";
+
+function time() {
+    return moment().tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss');
+}
+
+const logger = {
+    info: function (msg, who) {
+        console.log(chalk.blue(`[${time()}] [${who}] ${msg}`))
+    },
+
+    ok: function (msg, who) {
+        console.log(chalk.green(`[${time()}] [${who}] ${msg}`))
+    },
+
+    warn: function (msg, who) {
+        console.log(chalk.yellow(`[${time()}] [${who}] ${msg}`))
+    },
+
+    err: function (msg, who) {
+        console.log(chalk.red(`[${time()}] [${who}] ${msg}`))
+    },
+
+    ing: function (msg, who) {
+        console.log(chalk.cyan(`[${time()}] [${who}] ${msg}`))
+    }
+}
+
+module.exports = logger;
